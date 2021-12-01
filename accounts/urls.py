5b3c0dc771a75a047a7bd905333cc67bcbad5644
Martin Yanchev <myanch200@gmail.com>
2021-12-01 20:25:14 +0000

    update: Adding functionalities to the navbar

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import user_register, home, user_login
+from .views import user_register, home, user_login, user_logout
 app_name = 'accounts'
 urlpatterns = [
     path('',home, name='home'),
     path('register/', user_register, name='register'),
     path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
 ]
