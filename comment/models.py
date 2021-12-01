16dcbe116f01f2b930aa1ea8f7fae64552153123
Martin Yanchev <myanch200@gmail.com>
2021-12-01 23:43:58 +0000

    update: adding html for comment section

--- comment/models.py
+++ comment/models.py
@@ -9,3 +9,6 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    def __str__(self):
+        return f"{self.plan.title} - {self.author.username}"
+    
--- plan/views.py
+++ plan/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Plan , Image
 from comment.forms import CommentForm
+from comment.models import Comment
 # Create your views here.
 def detailed_plan(request, plan_id):
     form = CommentForm()
     plan = get_object_or_404(Plan, pk=plan_id)
     images = Image.objects.filter(plan=plan)
+    comments = Comment.objects.filter(plan=plan)
     context ={
             'plan': plan,
             'images' : images,
-            'form': form}
+            'form': form,
+            'comments': comments}
     return render(request, 'plan/detailed_plan.html', context)
